0c4c6089297ca26e6768463322aaa56d625a9196
Jeremy Kun <j2kun@users.noreply.github.com>
2018-07-27 22:07:59 -0700

    add another test case

--- tessellation_graph_test.py
+++ tessellation_graph_test.py
@@ -99,18 +99,20 @@ def test_three_layer_tessellation_graph_6_4():
     assert_edges_are_exactly(graph, edges)
 
 
-def test_three_layer_tessellation_graph_3_7():
-    graph = TessellationGraph(TessellationConfiguration(3, 7), num_layers=3)
-    assert len(graph.layers) == 3
+def test_four_layer_tessellation_graph_3_7():
+    graph = TessellationGraph(TessellationConfiguration(3, 7), num_layers=4)
+    assert len(graph.layers) == 4
     assert len(graph.layers[0]) == 1
     assert len(graph.layers[1]) == 15
     assert len(graph.layers[2]) == 45
-    assert len(graph.vertices) == 1 + 15 + 45
+    assert len(graph.layers[3]) == 120
+    assert len(graph.vertices) == 1 + 15 + 45 + 120
 
     all_vertices = [(n.layer, n.index_in_layer) for n in graph.vertices]
 
     within_second_layer = [((1, i), (1, (i + 1) % 15)) for i in range(15)]
     within_third_layer = [((2, i), (2, (i + 1) % 45)) for i in range(45)]
+    within_fourth_layer = [((3, i), (3, (i + 1) % 120)) for i in range(120)]
     first_layer_to_second_layer = [
         ((0,0), (1,0)),
         ((0,0), (1,5)),
@@ -130,6 +132,41 @@ def test_three_layer_tessellation_graph_3_7():
         ((1,12), (2,34)),
         ((1,13), (2,38)),
         ((1,14), (2,42)),
+    ]
+
+    third_layer_to_fourth_layer = [
+        ((2,1), (3,0)),
+        ((2,2), (3,4)),
+        ((2,3), (3,8)),
+        ((2,5), (3,11)),
+        ((2,6), (3,15)),
+        ((2,7), (3,19)),
+        ((2,9), (3,22)),
+        ((2,10), (3,26)),
+        ((2,11), (3,30)),
+        ((2,13), (3,33)),
+        ((2,14), (3,37)),
+        ((2,16), (3,40)),
+        ((2,17), (3,44)),
+        ((2,18), (3,48)),
+        ((2,20), (3,51)),
+        ((2,21), (3,55)),
+        ((2,22), (3,59)),
+        ((2,24), (3,62)),
+        ((2,25), (3,66)),
+        ((2,28), (3,73)),
+        ((2,29), (3,77)),
+        ((2,31), (3,80)),
+        ((2,32), (3,84)),
+        ((2,33), (3,88)),
+        ((2,35), (3,91)),
+        ((2,36), (3,95)),
+        ((2,37), (3,99)),
+        ((2,39), (3,102)),
+        ((2,40), (3,106)),
+        ((2,41), (3,110)),
+        ((2,43), (3,113)),
+        ((2,44), (3,117)),
     ]
 
     edges = set(within_second_layer + within_third_layer
