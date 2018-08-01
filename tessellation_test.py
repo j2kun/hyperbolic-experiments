1d74f6f3271abe00ba22875098fface70964c874
Jeremy Kun <j2kun@users.noreply.github.com>
2018-08-01 08:54:30 -0700

    refactor testing utils

--- tessellation_test.py
+++ tessellation_test.py
@@ -1,31 +1,10 @@
 from assertpy import assert_that
-from geometry import EPSILON
 from tessellation_graph import TessellationConfiguration
 import itertools
 import math
 
 from tessellation import *
-
-
-def is_close(v1, v2):
-    try:
-        return v1.is_close_to(v2)
-    except AttributeError:
-        return abs(v1 - v2) < EPSILON
-
-
-def assert_are_close(v1, v2):
-    assert_that(is_close(v1, v2)).is_true()
-
-
-def assert_iterables_are_close(s1, s2):
-    # two iterables are close if each item in one is close to some item
-    # in the other
-    for item in s1:
-        assert_that(any(is_close(item, x) for x in s2)).is_true()
-
-    for item in s2:
-        assert_that(any(is_close(item, x) for x in s1)).is_true()
+from testing import *
 
 
 def test_fundamental_triangle():
