3d9c02e9b7193990932f90a17398475067e36f0d
Jeremy Kun <j2kun@users.noreply.github.com>
2018-08-01 17:55:18 -0700

    add method to compute the center polygon

--- tessellation.py
+++ tessellation.py
@@ -97,10 +97,29 @@ class HyperbolicTessellation(object):
     arcs of circles perpendicular to the boundary of the disk.
     """
 
-    def __init__(self,
-            configuration,
-            num_layers=5):
+    def __init__(self, configuration):
         self.configuration = configuration
+        self.disk_model = PoincareDiskModel(Point(0, 0), radius=1)
+
         center, top_vertex, x_axis_vertex = compute_fundamental_triangle(configuration)
 
         # compute the vertices of the center polygon via reflection
+        self.center_polygon = self.compute_center_polygon(center, top_vertex, x_axis_vertex)
+
+    def compute_center_polygon(self, center, top_vertex, x_axis_vertex):
+        p = self.configuration.numPolygonSides
+
+        """The center polygon's first vertex is the top vertex (the one that
+        makes an angle of pi / q), because the x_axis_vertex is the center of
+        an edge.
+        """
+        polygon = [top_vertex]
+
+        p1, p2 = top_vertex, x_axis_vertex
+        for i in range(p):
+            p2 = self.disk_model.line_through(center, p1).reflect(p2)
+            polygon.append(p2)
+            p1 = self.disk_model.line_through(center, p2).reflect(p1)
+
+        assert len(polygon) == p
+        return p
